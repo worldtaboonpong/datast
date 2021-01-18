2987da5252911bb3bd168631def281afbcf0872a
World <catsfinetoo@gmail.com>
2021-01-18 11:05:13 +0700

    do clustering without index column

--- Clustering/clustering.py
+++ Clustering/clustering.py
@@ -3,16 +3,17 @@ from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 
 df = pd.read_excel('C:/Users/World/Documents/SeniorProject/datast/harmful30jun2020.xls')
-# num_cluster=3
 range_n_clusters = list (range(2,10))
 scores = []
 
 dataTypeDict = dict(df.dtypes)
 
 for key in dataTypeDict:
-    if (dataTypeDict[key] != 'int64'):
-        df.drop(key, inplace=True, axis=1)
+    # Check if column is not integer or columnn is index column sorted
+    if (dataTypeDict[key] != 'int64' or df[key].is_monotonic):
+        df.drop(key, inplace=True,axis=1)
 
+# Do Silhouette Method to find best K to fit
 for n_clusters in range_n_clusters:
     kmeans = KMeans(n_clusters=n_clusters)
     new = df._get_numeric_data()
@@ -20,7 +21,7 @@ for n_clusters in range_n_clusters:
     predict=kmeans.predict(new)
     score = silhouette_score(df, predict)
     scores.append(score)
-    # print("For n_clusters = {}, silhouette score is {})".format(n_clusters, score))
+    
 
 best_cluster = range_n_clusters[scores.index(max(scores))]
 kmeans = KMeans(n_clusters=best_cluster)
@@ -33,9 +34,5 @@ df_kmeans['Cluster KMeans'] = pd.Series(predict, index=df_kmeans.index)
 
 print(df_kmeans)
 
-# df_kmeans = df.copy(deep=True)
-# df_kmeans['Cluster KMeans'] = pd.Series(predict, index=df_kmeans.index)
-
-# print(df_kmeans)
 
 
