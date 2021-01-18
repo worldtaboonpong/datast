eecd07c924540101dc8a07f4d0728737777f376e
World <catsfinetoo@gmail.com>
2021-01-18 08:47:12 +0700

    using k-means which k comes from Silhouette method

--- Clustering/clustering.py
+++ Clustering/clustering.py
@@ -5,6 +5,7 @@ from sklearn.metrics import silhouette_score
 df = pd.read_excel('C:/Users/World/Documents/SeniorProject/datast/harmful30jun2020.xls')
 # num_cluster=3
 range_n_clusters = list (range(2,10))
+scores = []
 
 dataTypeDict = dict(df.dtypes)
 
@@ -18,9 +19,19 @@ for n_clusters in range_n_clusters:
     kmeans.fit(new)
     predict=kmeans.predict(new)
     score = silhouette_score(df, predict)
-    print("For n_clusters = {}, silhouette score is {})".format(n_clusters, score))
+    scores.append(score)
+    # print("For n_clusters = {}, silhouette score is {})".format(n_clusters, score))
 
-print(range_n_clusters[1])
+best_cluster = range_n_clusters[scores.index(max(scores))]
+kmeans = KMeans(n_clusters=best_cluster)
+new = df._get_numeric_data()
+kmeans.fit(new)
+predict=kmeans.predict(new)
+df_kmeans = df.copy(deep=True)
+df_kmeans['Cluster KMeans'] = pd.Series(predict, index=df_kmeans.index)
+
+
+print(df_kmeans)
 
 # df_kmeans = df.copy(deep=True)
 # df_kmeans['Cluster KMeans'] = pd.Series(predict, index=df_kmeans.index)
