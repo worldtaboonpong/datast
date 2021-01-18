ed921302098d4b77786a575f510c73eb242ba949
World <catsfinetoo@gmail.com>
2021-01-18 08:36:39 +0700

    try select K from Silhouette method

--- Clustering/clustering.py
+++ Clustering/clustering.py
@@ -1,8 +1,10 @@
 import pandas as pd 
 from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
 
 df = pd.read_excel('C:/Users/World/Documents/SeniorProject/datast/harmful30jun2020.xls')
-num_cluster=3
+# num_cluster=3
+range_n_clusters = list (range(2,10))
 
 dataTypeDict = dict(df.dtypes)
 
@@ -10,30 +12,19 @@ for key in dataTypeDict:
     if (dataTypeDict[key] != 'int64'):
         df.drop(key, inplace=True, axis=1)
 
-kmeans = KMeans(n_clusters=num_cluster,random_state=1)
-new = df._get_numeric_data()
-kmeans.fit(new)
-predict=kmeans.predict(new)
-df_kmeans = df.copy(deep=True)
-df_kmeans['Cluster KMeans'] = pd.Series(predict, index=df_kmeans.index)
+for n_clusters in range_n_clusters:
+    kmeans = KMeans(n_clusters=n_clusters)
+    new = df._get_numeric_data()
+    kmeans.fit(new)
+    predict=kmeans.predict(new)
+    score = silhouette_score(df, predict)
+    print("For n_clusters = {}, silhouette score is {})".format(n_clusters, score))
 
-print(df_kmeans)
+print(range_n_clusters[1])
+
+# df_kmeans = df.copy(deep=True)
+# df_kmeans['Cluster KMeans'] = pd.Series(predict, index=df_kmeans.index)
+
+# print(df_kmeans)
 
 
-# df_train.loc[:,'clusters'] = kmeans.labels_
-
-
-
-# X = pd.DataFrame({'id': [1,2,3,4,5],
-#                   'value_1': [1,3,1,4,5],
-#                   'value_2': [0,0,1,5,0]})
-
-# # Split ALL columns
-# (X_train, X_test) = train_test_split(X,test_size=0.30)
-# # Cluster using SOME columns
-# kmeans = KMeans(n_clusters=2, n_init=3, max_iter=3000, random_state=1)
-# kmeans = kmeans.fit(X_train[['value_1','value_2']])
-# # Save the labels
-# X_train.loc[:,'labels'] = kmeans.labels_
-
-# print(X_train)
