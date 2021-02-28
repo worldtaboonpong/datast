14d5d19ff736274d195c67b1dbea212cb728c3e5
World <catsfinetoo@gmail.com>
2021-02-28 16:45:26 +0700

    init flask app

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
+from flask import Flask
+from flask import render_template
 from Clustering.func_clustering import clustering 
 from Statistics.StatisticFunction import statistics
 from AssociationRule.assocrule import association
 
 import pandas as pd
 
-df = pd.read_excel('./MRTuser.xlsx' , 'สายฉลองรัชธรรม'  )
+app = Flask(__name__)
 
-qa_clustering = clustering(df)
-qa_statistic = statistics(df)
-qa_assoocrule = association(df)
-qa={**qa_clustering,**qa_statistic,**qa_assoocrule}
+# df = pd.read_excel('./MRTuser.xlsx' , 'สายฉลองรัชธรรม'  )
 
-keyList = list(qa.keys())
 
-print(keyList)
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+@app.route('/analyze')
+def analyze():
+    # qa_clustering = clustering(df)
+    # qa_statistic = statistics(df)
+    # qa_assoocrule = association(df)
+    # qa={**qa_clustering,**qa_statistic,**qa_assoocrule}
+
+    return
+
+if __name__ == '__main__':
+    app.run(debug=True)
