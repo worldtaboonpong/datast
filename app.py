c5a8b530529e7c10173a42bd78749f7571a00031
World <catsfinetoo@gmail.com>
2021-05-04 14:46:53 +0700

    prepare for deployment

--- app.py
+++ app.py
@@ -157,4 +157,5 @@ def showOutput():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port,debug=True)
